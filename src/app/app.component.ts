46459bff626997648722e79cbfae3ef44a07fe4c
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-05-17 08:45:51 -0600

    changes to file structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: './templates/app.html'
+})
+
+export class AppComponent {
+	title = 'Tour of Heroes';
+}
